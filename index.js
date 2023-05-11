147811fdaee4a770ab4ea3f5f923eb12047612bb
Ariabayu <Bayuaria33@gmail.com>
2023-05-11 10:52:42 +0700

    attempt 1 @ fixing CORS error ¯\_(ツ)_/¯

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const xss = require("xss-clean");
 const app = express();
 const port = 4000;
 const mainRoute = require(`./src/routes/index`);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(
   cors({
     origin: "*",
